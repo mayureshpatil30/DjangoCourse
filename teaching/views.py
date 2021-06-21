85faf1bc6abc938bbbf81a77ef6a1e125bd3db44
mayuresh <mayuresh3097@gmail.com>
2021-06-21 18:19:24 +0530

    registration

--- teaching/views.py
+++ teaching/views.py
@@ -88,6 +88,7 @@ class InstituteRegisterView(FormView):
 
 
     
+    
 # ----------------------------------registration for Teacher------------------------------------------    
 # in template use hidden field reg_type
 #('T', 'S', 'I')
